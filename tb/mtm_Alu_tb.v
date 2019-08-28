d6ed344af1f500baaf91fb4cb64056a312447d26
smastein <40031383+smastein@users.noreply.github.com>
2019-08-28 22:52:28 +0200

    Add files via upload

--- tb/mtm_Alu_tb.v
+++ tb/mtm_Alu_tb.v
@@ -32,4 +32,100 @@ module mtm_Alu_tb (
     output reg sout
 ) ;
 
+	integer i,j;
+	localparam  AND = 3'b000,
+				OR = 3'b001,
+				ADD = 3'b100,
+				SUB = 3'b101;
+
+	task send_byte 
+		input [7:0] data;
+		input [1:0] CMD; //=0 - Data, =1 - CTL
+		begin
+			for(i=0; i<11; i= i+1) begin
+				@(posedge clk); begin
+					if(i == 0) begin
+						sin = 0;
+					end
+					else if(i == 10) begin
+						sin = 1;
+					end
+					else if(i == 1 && CMD == 0) begin
+						sin = 0;
+					end
+					else if(i == 1 && CMD == 1) begin
+						sin = 1;
+					end
+					else begin
+						sin = data[i-2];
+					end
+				end
+			end
+		end
+	endtask
+
+	task send_calculation_data
+		input [71:0] data;
+		begin
+			for(j=0; j<9; j=j+1) begin
+				@(posedge clk); begin
+					if (j == 0) begin
+						send_byte(byte[31:24],0);
+					end
+					else if (j == 1) begin
+						send_byte(byte[23:16],0);
+					end
+					else if (j == 2) begin
+						send_byte(byte[15:8],0);
+					end
+					else if (j == 3) begin
+					  send_byte(byte[7:0],0);
+					end
+					else if (j == 4) begin
+					  send_byte(byte[63:56],0);
+					end
+					else if (j == 5) begin
+					  send_byte(byte[55:48],0);
+					end
+					else if (j == 6) begin
+					  send_byte(byte[47:40],0);
+					end
+					else if (j == 7) begin
+					  send_byte(byte[39:32],0);
+					end
+					else if (j == 8) begin
+					  send_byte(byte[71:64],1);
+					end
+				end
+			end
+		end
+	endtask
+
+	task compare
+		input [39:0] result;
+		input [31:0] A;
+		input [31:0] B;
+		input [7:0] CTL;
+		reg [32:0] expected;
+		output solution;
+		begin
+			case(CTL[6:4])
+				AND: begin
+					expected = A & B;
+				end
+				OR: begin
+					expected = A | B;
+				end
+				ADD: begin
+					expected = A + B;
+				end
+				SUB: begin
+					expected = A - B;
+				end
+			endcase
+			
+			solution = expected == {result[6],rsult[39:8]} // {CARRY, A+B}
+		end
+	endtask
+
 endmodule
